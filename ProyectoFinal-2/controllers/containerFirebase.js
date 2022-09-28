f65f38274c8e39371ccb75bd11ce41b3e74bd32a
emispec <emisebastian86@gmail.com>
2022-09-28 19:36:11 -0300

    PF2

---
+++ ProyectoFinal-2/controllers/containerFirebase.js
@@ -0,0 +1,46 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../backend22-37d94-firebase-adminsdk-q9x8g-51d729c7bb.json");
+
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+const { getFirestore, doc, getDoc } = require("firebase-admin/firestore");
+
+class Container {
+  constructor() {
+    this.db = getFirestore();
+  }
+
+  save(obj) {
+    try {
+      return this.db.collection("products").add(obj);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getById(id) {
+    try {
+      const data = this.db.doc(`/products/${id}`).get();
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getAll() {
+    try {
+      return this.model.find();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  deleteById(id) {
+    try {
+      return this.model.findByIdAndDelete(id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = Container;
